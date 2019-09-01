890448069ef5fc30c8d0c4c0a9ecd19a6c44a455
Ondřej Měkota <ondrej.mekota@me.com>
2019-09-01 09:24:59 +0200

    list template works

--- python/docstring.py
+++ python/docstring.py
@@ -24,25 +24,23 @@ def substitute_list(list_name, template, list_):
         if match:
             list_to_substitute, old_line = match.groups()
             if list_to_substitute == list_name:
+                new_lines = []
                 for item in list_:
                     new_line = re.sub('(\$\w+)', item, old_line)
-                    result_lines.append(new_line)
-            else:
-                result_lines.append(line)
-
-
-
-
+                    new_line = bytes(new_line, 'utf-8').decode('unicode_escape')
+                    new_lines.append(new_line)
+                result_lines.append(''.join(new_lines))
+        else:
+            result_lines.append(line)
+    return '\n'.join(result_lines)
 
 def google_template(arguments, funcdef_indent, indent):
     with open('./google.txt', 'r') as f:
         template = Template(f.read())
-    return template.safe_substitute(i=funcdef_indent, i2=indent)
+    list_not_sub = template.safe_substitute(i=funcdef_indent, i2=indent)
+    return substitute_list('args', list_not_sub, arguments)
 
 if __name__ == '__main__':
-    # x = '     def foo(a=25, b=10, c = \'ahoj ): #\\\'svete\\\'\'):  # comme ):  # {} "" \' \'\\\'nt    '
-    # ret = re.findall('(.*:)\s*#.*', x)
-    # ret = re.findall('(=)(\d)', x)
     print(google_template(['arg1','arg2'],' '*4, ' '*4))
 
 
