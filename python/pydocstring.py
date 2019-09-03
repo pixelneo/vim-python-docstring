4c84de74d3030a28442b7ddf5a2c5acd19a48f20
Ondřej Měkota <ondrej.mekota@me.com>
2019-09-03 12:45:51 +0200

    x

--- python/pydocstring.py
+++ python/pydocstring.py
@@ -113,19 +113,6 @@ class Templater:
                 result_lines.append(line)
         return '\n'.join(result_lines)
 
-    v"""
-    v
-    v
-    v
-    vArgs:
-    vvself: ()
-    vvfuncdef_indent: ()
-    vvarguments: ()
-
-    vReturns:
-    vv
-    v
-    v"""
 
     def get_template(self, funcdef_indent, arguments):
         print(self.indent)
