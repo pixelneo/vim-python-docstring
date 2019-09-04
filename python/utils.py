b5b412158800c27e2969d8d5ff49e9a0a5ab6f14
Ondřej Měkota <ondrej.mekota@me.com>
2019-09-04 16:07:50 +0200

    add utils

---
+++ python/utils.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class NoValue(Enum):
+     def __repr__(self):
+         return '<%s.%s>' % (self.__class__.__name__, self.name)
+
+class ObjectType(NoValue):
+    METHOD = auto()
+    CLASS = auto()
